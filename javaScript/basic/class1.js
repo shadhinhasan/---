6c03b0456136f2cb79c4cde19ed06593eeebd0e0
Hasanuzzaman Shadhin <shadhinhasan.cse@gmail.com>
2023-06-27 08:37:02 +0600

    javascript

---
+++ javaScript/basic/class1.js
@@ -0,0 +1,75 @@
+// Variables
+// array
+// loop
+// function
+
+// keyword variable_name = value
+// var name = 'sdfi';
+// var age = 30;
+// var bottle = 'shorbot';
+
+// bottle = "mango juice";
+// // bottle='water';
+ 
+// var bati = 'alu vaji';
+
+// var vatNewaBag = 'tiffin box';
+// var vatNewabag = 'vat gosto box';
+// var product_name = "T shirt";
+// var product_price = 500;
+
+
+// console.log(vatNewaBag);
+// console.log(vatNewabag);
+// // console.log(bati);
+// // console.log(bottle);
+// console.log(product_name);
+// console.log('Price: ',product_price);
+
+//যতক্ষন কোন variable এর মধ্যে কোন value assign na hoy totokkhon er moddhe "undefined" thake
+
+
+
+var first_name = " sahadat";
+var last_name = "Hossen";
+var age='';
+var balance = 500;
+var married = false;
+var test;
+var test2 = null;
+
+// var x =200;
+// var y = 30;
+
+// var result = x%y;
+// console.log(result);
+
+// console.log(typeof(age));
+// console.log(first_name + last_name);
+// console.log(age + balance);
+// console.log(first_name + balance);
+// var output = "I am " + first_name + " " + last_name + ", I am " + age + " year's old!";
+console.log("I am " + first_name + " " + last_name + ", I am " + age + " year's old!");
+
+var x = 20;
+
+console.log(typeof(x));  
+
+console.log('-----------Operator-------------');
+
+// + jog kore
+// -  
+// *  
+// /  
+// %  vagsesh nirnoy kore
+// == dui pashe soman ki na check kore
+// >= বাম পাশের টা ডান পাশের চেয়ে বড় কি না তাই চেক করে
+// <= বাম পাশের টা ডান পাশের চেয়ে ছোট কি না তাই চেক করে
+// != বাম পাশের টা ডান পাশের অসমান কি না তাই চেক করে
+// ! true থাকলে false করে, false thakle true kore, মোটকথা যাই থাকুক তার উল্টা করে
+// ++ operand er man 1 kore incriment kore
+// --  operand er man 1 kore dicriment kore
+// ===  দুই পাশের মান সমান কি না তাই চেক করে
+// !==  দুই পাশের মান অসমান কি না তাই চেক করে
+// &&  দুই পাশের শর্ত true হলে, true return kore na hole false return kore
+// ||  দুই পাশের condition false hole false অন্যথায় true return kore
---
+++ javaScript/class-2.js
@@ -0,0 +1,77 @@
+// console.log('fdug')
+// console.log('shadhin')
+// console.log('shadhin')
+// console.log('shadhin')
+// console.log('shadhin')
+
+// for (let i = 1; i <= 1000; i++) {
+//     console.log('135 X '+ i +' = ' + 135*i)
+//     console.log('Ami valo aci')
+// }
+
+// let text1 = "John";
+// let text2 = "Doe";
+// let text3 = text1 + " " + text2;
+// document.getElementById("demo").innerHTML = text3;
+
+// let text1 = "What a very ";
+// text1 += "nice day";
+// document.getElementById("demo2").innerHTML = text1;
+
+// let x = 5 + 5;
+// let y = "55" + 5;
+// let z = "Hello" + 5;
+// document.getElementById("demo").innerHTML =
+// x + "<br>" + y + "<br>" + z;
+
+// console.log(typeof x)
+// console.log(typeof y)
+// console.log(typeof z)
+
+// let x = 5;
+// let y = 6;
+// let z = 7;
+
+// let myObject = { x: 5, y: 6, z: 7 }
+
+// console.log(typeof myObject)
+
+// function sleep(name){
+//     console.log( name + "is sleeping from 10 pm")
+// }
+// sleep("shadhin");
+// sleep("hasan");
+// sleep("josim");
+
+// function sleep(name, time){
+//     console.log( name + " is sleeping from " + time)
+// }
+// sleep("shadhin", "10 pm");
+// sleep("hasan", "9 pm");
+// sleep("josim", "11 pm");
+// console.log("I am a student")
+
+// console.log(a)
+// console.log(print)
+
+// var a;
+// a= 10;
+
+// function print() {
+//     console.log("Hi there!!!!")
+// }
+
+// console.log(a);
+
+// var b;
+// b = "shadhin"
+
+// console.log(print)
+
+// console.log(b)
+
+
+// var x;
+// console.log(x);
+// x = "text";
+// console.log(x);
